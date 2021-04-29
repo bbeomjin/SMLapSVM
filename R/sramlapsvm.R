2d0edb743cafffbabb54420c1bf04c9be3c0e265
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2021-04-29 18:58:35 +0900

    Update sramlapsvm.R

--- R/sramlapsvm.R
+++ R/sramlapsvm.R
@@ -343,7 +343,7 @@ find_theta.sramlapsvm = function(y, anova_kernel, L, cmat, c0vec, gamma, n_class
 
   Dmat = c(Dmat, c(rep(0, n_l * n_class)))
   Dmat = diag(Dmat)
-  Dmat = fixit(Dmat, epsilon = eig_tol_D)
+  Dmat = fixit(Dmat, epsilon = eig_tol_D) + 1e-6
 
   dvec_temp = matrix(1 - gamma, nrow = n_l, ncol = n_class)
   dvec_temp[cbind(1:n_l, y)] = gamma
@@ -521,7 +521,7 @@ sramlapsvm_core = function(anova_K, L, theta, y, gamma = 0.5, lambda, lambda_I, 
   KLK = n_l * lambda * K + m_mat
   # KLK = (KLK + t(KLK)) / 2
 
-  KLK = fixit(KLK, epsilon = eig_tol_I)
+  KLK = fixit(KLK, epsilon = eig_tol_I) + 1e-6
   # diag(KLK) = diag(KLK) + epsilon_D
   inv_KLK = Matrix::solve(KLK)
 
@@ -552,7 +552,7 @@ sramlapsvm_core = function(anova_K, L, theta, y, gamma = 0.5, lambda, lambda_I, 
   # D = fixit(D)
   # max_D = max(abs(D))
   # D = D / max_D
-  D = fixit(D, epsilon = eig_tol_D)
+  D = fixit(D, epsilon = eig_tol_D) + 1e-6
   # diag(D) = diag(D) + epsilon_D
 
 
