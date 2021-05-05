a552553b58bacd511888f6a803b4b32fe22e8ca5
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2021-05-05 19:33:29 +0900

    Update sramlapsvm.R

--- R/sramlapsvm.R
+++ R/sramlapsvm.R
@@ -482,9 +482,9 @@ sramlapsvm_core = function(anova_K, L, theta, y, gamma = 0.5, lambda, lambda_I, 
   K = combine_kernel(anova_K, theta = theta)
   # K = (K + t(K)) / 2
 
-  # if (sum(K) == 0) {
-  #   diag(K) = 1
-  # }
+  if (sum(K) == 0) {
+    diag(K) = 1
+  }
 
   n = nrow(K)
   n_l = length(y)
