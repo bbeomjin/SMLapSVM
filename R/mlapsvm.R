5b0e03d241dc9be9965063e90972504f395947de
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2021-05-06 12:59:55 +0900

    update

--- R/mlapsvm.R
+++ R/mlapsvm.R
@@ -1,4 +1,5 @@
-mlapsvm_compact = function(K, L, y, lambda, lambda_I, epsilon = 1e-6, eig_tol_D = 2e-15, eig_tol_I = 2e-15, epsilon_D = 1e-13)
+mlapsvm_compact = function(K, L, y, lambda, lambda_I, epsilon = 1e-6,
+                           eig_tol_D = .Machine$double.eps, eig_tol_I = .Machine$double.eps, epsilon_D = 1e-13)
 {
 
   # The sample size, the number of classes and dimension of QP problem
@@ -171,7 +172,7 @@ mlapsvm_compact = function(K, L, y, lambda, lambda_I, epsilon = 1e-6, eig_tol_D 
 
 
 mlapsvm = function(x = NULL, y, ux = NULL, lambda, lambda_I, kernel, kparam, scale = FALSE, adjacency_k = 6, normalized = FALSE,
-                   weight = NULL, weightType = "Binary", epsilon = 1e-6, eig_tol_D = 2e-15, eig_tol_I = 2e-15, epsilon_D = 1e-13)
+                   weight = NULL, weightType = "Binary", epsilon = 1e-6, eig_tol_D = .Machine$double.eps, eig_tol_I = .Machine$double.eps, epsilon_D = 1e-13)
 {
   out = list()
   n_l = NROW(x)
