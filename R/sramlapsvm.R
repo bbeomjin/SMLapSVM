5ef96b34955623fd5af0620ed076d0b4ace29d44
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2021-05-23 21:24:03 +0900

    update

--- R/sramlapsvm.R
+++ R/sramlapsvm.R
@@ -295,7 +295,7 @@ theta_step.sramlapsvm = function(object, lambda_theta_seq = 2^{seq(-10, 10, leng
 
 
 find_theta.sramlapsvm = function(y, anova_kernel, L, cmat, c0vec, gamma, n_class, lambda, lambda_I, lambda_theta = 1,
-                                 eig_tol_D = 0, eig_tol_I = 1e-15, epsilon_D = 1e-8, epsilon_I = 0)
+                                 eig_tol_D = 0, eig_tol_I = .Machine$double.eps, epsilon_D = 1e-8, epsilon_I = 0)
 {
 
   if (anova_kernel$numK == 1)
@@ -393,7 +393,7 @@ find_theta.sramlapsvm = function(y, anova_kernel, L, cmat, c0vec, gamma, n_class
 
 
 sramlapsvm_compact = function(anova_K, L, theta, y, gamma = 0.5, lambda, lambda_I, epsilon = 1e-6,
-                              eig_tol_D = 0, eig_tol_I = 1e-15, epsilon_D = 1e-8, epsilon_I = 0)
+                              eig_tol_D = 0, eig_tol_I = .Machine$double.eps, epsilon_D = 1e-8, epsilon_I = 0)
 {
 
   out = list()
