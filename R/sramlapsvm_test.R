8f569147bf3c6341d18d36d3126f93aa7090e755
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2021-08-14 19:22:11 +0900

    update

--- R/sramlapsvm_test.R
+++ R/sramlapsvm_test.R
@@ -477,8 +477,8 @@ sramlapsvm_compact2 = function(anova_K, L, theta, y, gamma = 0.5, lambda, lambda
   # inv_K_KLK = solve(K_KLK, tol = eig_tol_I) %*% K %*% t(J)
   # inv_K_KLK = solve(K_KLK, K %*% t(J), tol = eig_tol_I)
 
-  # inv_K_KLK = chol2inv(chol(K_KLK))
-  inv_K_KLK = solve(K_KLK, tol = eig_tol_I)
+  inv_K_KLK = chol2inv(chol(K_KLK))
+  # inv_K_KLK = solve(K_KLK, tol = eig_tol_I)
   inv_K_KLK = (inv_K_KLK + t(inv_K_KLK)) / 2
   inv_K_KLK = inv_K_KLK %*% K %*% t(J)
 
