23518c9353e6808078d5a51979e4a60f42dc983a
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2022-01-11 22:26:12 +0900

    update

--- R/sramlapsvm.R
+++ R/sramlapsvm.R
@@ -642,7 +642,7 @@ sramlapsvm_compact = function(anova_K, L, theta, y, gamma = 0.5, lambda, lambda_
   lambda_KLK = n_l * lambda_I / n^2 * KLK
 
   K_KLK = lambda_K + lambda_KLK
-  # K_KLK = (K_KLK + t(K_KLK)) / 2
+  K_KLK = (K_KLK + t(K_KLK)) / 2
 
   inv_K_KLK = solve(K_KLK, tol = eig_tol_I)
   # inv_K_KLK = (inv_K_KLK + t(inv_K_KLK)) / 2
@@ -661,6 +661,7 @@ sramlapsvm_compact = function(anova_K, L, theta, y, gamma = 0.5, lambda, lambda_
     D = D + Hmatj[[j]] %*% Q %*% t(Hmatj[[j]])
     Amat[, j] = -Lmatj[[j]]
   }
+  D = (t(D) + D) / 2
   D = fixit(D, epsilon = eig_tol_D)
   max_D = max(abs(D))
   # D = D / max_D
