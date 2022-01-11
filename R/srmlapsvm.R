4e8ac5468e7e4752c68bb2f9b51f2814ed44a75c
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2022-01-11 22:05:42 +0900

    update

--- R/srmlapsvm.R
+++ R/srmlapsvm.R
@@ -621,10 +621,10 @@ srmlapsvm_compact = function(anova_K, L, theta, y, gamma = 0.5, lambda, lambda_I
   lambda_KLK = n_l * lambda_I / n^2 * KLK
 
   K_KLK = lambda_K + lambda_KLK
-  # K_KLK = (K_KLK + t(K_KLK)) / 2
+  K_KLK = (K_KLK + t(K_KLK)) / 2
 
   inv_K_KLK = solve(K_KLK, tol = eig_tol_I)
-  inv_K_KLK = (inv_K_KLK + t(inv_K_KLK)) / 2
+  # inv_K_KLK = (inv_K_KLK + t(inv_K_KLK)) / 2
   inv_K_KLK = inv_K_KLK %*% K %*% t(J)
 
   Q = J %*% K %*% inv_K_KLK
