0064b616e464a7e42d374d78529b12a78e958013
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2022-01-17 02:10:01 +0900

    Update sramlapsvm.R

--- R/sramlapsvm.R
+++ R/sramlapsvm.R
@@ -667,7 +667,7 @@ sramlapsvm_compact = function(anova_K, L, theta, y, gamma = 0.5, lambda, lambda_
   # D = (D + t(D)) / 2
   D = fixit(D, epsilon = eig_tol_D)
   max_D = max(abs(D))
-  D = D / max_D
+  # D = D / max_D
   # diag(D) = diag(D) + epsilon_D
   diag(D) = diag(D) + max_D * epsilon_D
   #################################### for test #######################################
@@ -685,8 +685,8 @@ sramlapsvm_compact = function(anova_K, L, theta, y, gamma = 0.5, lambda, lambda_
   g_temp[y_index] = 1 - n_class
   g = as.vector(g_temp)
 
-  # dvec = -g
-  dvec = -g / max_D
+  dvec = -g
+  # dvec = -g / max_D
 
   # diag(Amat[(n_class + 1):(n_class + qp_dim), ]) = 1
   # diag(Amat[(n_class + qp_dim + 1):(n_class + 2 * qp_dim), ]) = -1
