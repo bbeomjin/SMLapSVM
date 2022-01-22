abd6ab90e7b13f8ab044e8421d1af2c16410cb37
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2022-01-22 21:15:46 +0900

    update

--- R/srmlapsvm_test.R
+++ R/srmlapsvm_test.R
@@ -625,7 +625,7 @@ srmlapsvm_compact2 = function(anova_K, L, theta, y, gamma = 0.5, lambda, lambda_
   
   K_KLK = lambda_K + lambda_KLK
   # K_KLK = (K_KLK + t(K_KLK)) / 2
-  diag(K_KLK) = diag(K_KLK) + max(abs(K_KLK)) * epsilon_I
+  # diag(K_KLK) = diag(K_KLK) + max(abs(K_KLK)) * epsilon_I
   
   # inv_K_KLK = solve(K_KLK, tol = eig_tol_I)
   inv_K_KLK = inverse(K_KLK, epsilon = eig_tol_I)
