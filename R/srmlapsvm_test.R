7f07a646ef0316c23648cca8cbc4c76e6ab6eea8
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2022-01-27 03:00:28 +0900

    update

--- R/srmlapsvm_test.R
+++ R/srmlapsvm_test.R
@@ -627,7 +627,7 @@ srmlapsvm_compact2 = function(anova_K, L, theta, y, gamma = 0.5, lambda, lambda_
 
   K_KLK = lambda_K + lambda_KLK
   # K_KLK = (K_KLK + t(K_KLK)) / 2
-  K_KLK = fixit(K_KLK, epsilon = 0)
+  K_KLK = fixit(K_KLK, epsilon = epsilon_I)
   # diag(K_KLK) = diag(K_KLK) + max(abs(K_KLK)) * epsilon_I
 
   JK = J %*% K
