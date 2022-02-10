d4caf3e2a2dc47b5c6ea553643ab6fed4c392d19
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2022-02-10 16:06:56 +0900

    update

--- R/srmlapsvm.R
+++ R/srmlapsvm.R
@@ -659,12 +659,13 @@ srmlapsvm_compact = function(anova_K, L, theta, y, gamma = 0.5, lambda, lambda_I
 
   JK = J %*% K
 
-  # inv_K_KLK = solve(K_KLK, tol = inv_tol)
+  inv_K_KLK = solve(K_KLK, tol = inv_tol)
   # inv_K_KLK = chol2inv(chol(K_KLK))
   # inv_K_KLK = inverse(K_KLK)
   # inv_K_KLK = (inv_K_KLK + t(inv_K_KLK)) / 2
+  inv_K_KLK = tcrossprod(inv_K_KLK, JK)
   # inv_K_KLK = inv_K_KLK %*% t(JK)
-  inv_K_KLK = solve(K_KLK, t(JK), tol = inv_tol)
+  # inv_K_KLK = solve(K_KLK, t(JK), tol = inv_tol)
   # inv_K_KLK = qr.solve(K_KLK, K %*% t(J), tol = eig_tol_I)
 
   Q = JK %*% inv_K_KLK
