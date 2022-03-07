f4408d6528055c49bb9f65858f5d4f396fb31c03
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2022-03-07 21:43:17 +0900

    update

--- R/subfuncs.R
+++ R/subfuncs.R
@@ -427,7 +427,7 @@ data_split = function(y, nfolds, seed = length(y))
 
 
 
-# fixit = function(A, epsilon = .Machine$double.eps) {
+# fixit3 = function(A, epsilon = .Machine$double.eps) {
 #
 #   if (!is.matrix(A)) {
 #     A = as.matrix(A)
@@ -482,7 +482,8 @@ fixit = function(A, epsilon) {
   }
 
   tau = max(0, delta - v)
-  A = eig$vectors %*% diag(v, d[1]) %*% t(eig$vectors) + diag(tau, d[1])
+  A = eig$vectors %*% diag(v, d[1]) %*% t(eig$vectors)
+  diag(A) = diag(A) + tau
   # A = eig$vectors %*% ((v + tau) * t(eig$vectors))
   return(A)
 }
@@ -610,23 +611,6 @@ fixit = function(A, epsilon) {
 #   return(A)
 # }
 
-# fixit8 = function(A, epsilon = .Machine$double.eps) {
-#
-#   if (!is.matrix(A)) {
-#     A = as.matrix(A)
-#   }
-#
-#   d = dim(A)
-#   eig = eigen(A, symmetric = TRUE)
-#   # eig = eigen(A)
-#   v = eig$values
-#
-#   delta = max(abs(v)) * epsilon
-#
-#   tau = pmax(0, delta + v)
-#   A = eig$vectors %*% diag(tau, d[1]) %*% t(eig$vectors)
-#   return(A)
-# }
 
 
 ##############################################################################################################################################
