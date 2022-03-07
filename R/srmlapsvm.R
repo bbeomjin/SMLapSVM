4ebd98c1b037e37e45e447e47c8114538b9deee5
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2022-03-07 22:35:24 +0900

    update

--- R/srmlapsvm.R
+++ R/srmlapsvm.R
@@ -680,15 +680,15 @@ srmlapsvm_compact = function(anova_K, L, theta, y, gamma = 0.5, lambda, lambda_I
   # KLK = fixit(KLK, epsilon = eig_tol_I)
 
   lambda_K = n_l * lambda * K
-  # lambda_K = fixit(lambda_K, epsilon = eig_tol_I)
-  diag(lambda_K) = diag(lambda_K) + epsilon_I
-  # diag(lambda_K) = diag(lambda_K) + max(abs(diag(lambda_K))) * epsilon_I
+  lambda_K = fixit(lambda_K, epsilon = eig_tol_I)
+  # diag(lambda_K) = diag(lambda_K) + epsilon_I
+  diag(lambda_K) = diag(lambda_K) + max(abs(diag(lambda_K))) * epsilon_I
 
   lambda_KLK = n_l * lambda_I / n^2 * KLK
-  # lambda_KLK = fixit(lambda_KLK, epsilon = eig_tol_I)
+  lambda_KLK = fixit(lambda_KLK, epsilon = eig_tol_I)
   # lambda_KLK = fixit(lambda_KLK)
   # diag(lambda_KLK) = diag(lambda_KLK) + epsilon_I
-  # diag(lambda_KLK) = diag(lambda_KLK) + max(abs(diag(lambda_KLK))) * epsilon_I
+  diag(lambda_KLK) = diag(lambda_KLK) + max(abs(diag(lambda_KLK))) * epsilon_I
 
   K_KLK = lambda_K + lambda_KLK
   # K_KLK = n_l * lambda * K + n_l * lambda_I / n^2 * KLK
