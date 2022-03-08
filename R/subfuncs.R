9e5f43aefd6ca338c91a22977354f1c50166eaab
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2022-03-08 19:58:45 +0900

    Update subfuncs.R

--- R/subfuncs.R
+++ R/subfuncs.R
@@ -443,7 +443,7 @@ data_split = function(y, nfolds, seed = length(y))
 #   return(A)
 # }
 
-fixit = function(A, epsilon) {
+fixit = function(A, epsilon = .Machine$double.eps) {
 
   if (!is.matrix(A)) {
     A = as.matrix(A)
@@ -453,22 +453,15 @@ fixit = function(A, epsilon) {
   eig = eigen(A, symmetric = TRUE)
   # eig = eigen(A)
   v = eig$values
-
-  if (missing(epsilon)) {
-    delta = max(abs(v)) * .Machine$double.eps
-  } else {
-    delta = epsilon
-  }
-
+  delta = max(abs(v)) * epsilon
   # delta = 2 * d[1] * max(abs(v)) * epsilon
-  # delta = max(abs(v)) * epsilon
   tau = max(0, delta - v)
   A = eig$vectors %*% diag(v + tau, d[1]) %*% t(eig$vectors)
   # A = eig$vectors %*% ((v + tau) * t(eig$vectors))
   return(A)
 }
 
-fixit2 = function(A, epsilon) {
+fixit2 = function(A, epsilon = .Machine$double.eps) {
 
   if (!is.matrix(A)) {
     A = as.matrix(A)
@@ -480,12 +473,8 @@ fixit2 = function(A, epsilon) {
   v = eig$values
 
   # delta = 2 * d[1] * max(abs(v)) * epsilon
-  if (missing(epsilon)) {
-    delta = max(abs(v)) * .Machine$double.eps
-  } else {
-    delta = epsilon
-  }
 
+  delta = max(abs(v)) * epsilon
   tau = max(0, delta - v)
   A = eig$vectors %*% diag(v, d[1]) %*% t(eig$vectors)
   diag(A) = diag(A) + tau
