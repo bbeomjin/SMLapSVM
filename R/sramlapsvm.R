f9a385342c874f36f2c6f61ea961d27cb5546dfe
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2022-03-14 13:29:18 +0900

    update

--- R/sramlapsvm.R
+++ R/sramlapsvm.R
@@ -682,22 +682,22 @@ sramlapsvm_compact = function(anova_K, L, theta, y, gamma = 0.5, lambda, lambda_
   # K = fixit(K, epsilon = eig_tol_I)
   # KLK = fixit(KLK, epsilon = eig_tol_I)
 
-  # lambda_K = n_l * lambda * K
-  # lambda_K = fixit(lambda_K, epsilon = eig_tol_I)
+  lambda_K = n_l * lambda * K
+  lambda_K = fixit(lambda_K, epsilon = eig_tol_I)
   # diag(lambda_K) = diag(lambda_K) + epsilon_I
   # diag(lambda_K) = diag(lambda_K) + max(abs(diag(lambda_K))) * epsilon_I
 
-  # lambda_KLK = n_l * lambda_I / n^2 * KLK
-  # lambda_KLK = fixit(lambda_KLK, epsilon = eig_tol_I)
+  lambda_KLK = n_l * lambda_I / n^2 * KLK
+  lambda_KLK = fixit(lambda_KLK, epsilon = eig_tol_I)
   # lambda_KLK = fixit(lambda_KLK)
   # diag(lambda_KLK) = diag(lambda_KLK) + epsilon_I
   # diag(lambda_KLK) = diag(lambda_KLK) + max(abs(diag(lambda_KLK))) * epsilon_I
 
-  # K_KLK = lambda_K + lambda_KLK
-  K_KLK = n_l * lambda * K + n_l * lambda_I / n^2 * KLK
+  K_KLK = lambda_K + lambda_KLK
+  # K_KLK = n_l * lambda * K + n_l * lambda_I / n^2 * KLK
   # K_KLK = (K_KLK + t(K_KLK)) / 2
-  K_KLK = fixit(K_KLK, epsilon = eig_tol_I)
-  diag(K_KLK) = diag(K_KLK) + max(abs(diag(K_KLK))) * epsilon_I
+  # K_KLK = fixit(K_KLK, epsilon = eig_tol_I)
+  # diag(K_KLK) = diag(K_KLK) + max(abs(diag(K_KLK))) * epsilon_I
   # diag(K_KLK) = diag(K_KLK) + epsilon_I
 
   JK = J %*% K
@@ -727,11 +727,11 @@ sramlapsvm_compact = function(anova_K, L, theta, y, gamma = 0.5, lambda, lambda_
     D = D + Hmatj[[j]] %*% Q %*% t(Hmatj[[j]])
     Amat[, j] = -Lmatj[[j]]
   }
-  # D = (D + t(D)) / 2
+  D = (D + t(D)) / 2
   D = fixit(D, epsilon = eig_tol_D)
   max_D = max(abs(diag(D)))
   # D = D / max_D
-  diag(D) = diag(D) + max_D * epsilon_D
+  # diag(D) = diag(D) + max_D * epsilon_D
   # diag(D) = diag(D) + epsilon_D
   #################################### for test #######################################
   # alpha_mat = matrix(rnorm(n_l * n_class), n_l, n_class)
