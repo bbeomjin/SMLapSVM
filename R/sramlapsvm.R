3e691ae4a175f479663b0d88c1e79e4dbfc8bf6e
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2022-03-20 13:46:02 +0900

    update

--- R/sramlapsvm.R
+++ R/sramlapsvm.R
@@ -578,8 +578,8 @@ find_theta.sramlapsvm = function(y, anova_kernel, L, cmat, c0vec, gamma, lambda,
   Dmat = c(Dmat, c(rep(0, n_l * n_class)))
   Dmat = diag(Dmat)
   # Dmat = fixit(Dmat, epsilon = eig_tol_D, is_diag = TRUE)
-  # diag(Dmat) = diag(Dmat) + max_D * epsilon_D
-  diag(Dmat) = diag(Dmat) + epsilon_D
+  diag(Dmat) = diag(Dmat) + max_D * epsilon_D
+  # diag(Dmat) = diag(Dmat) + epsilon_D
 
   # Dmat = fixit(Dmat, epsilon = eig_tol_D, is_diag = TRUE)
   # diag(Dmat) = diag(Dmat) + 1e-8
@@ -728,11 +728,11 @@ sramlapsvm_compact = function(anova_K, L, theta, y, gamma = 0.5, lambda, lambda_
     Amat[, j] = -Lmatj[[j]]
   }
   # D = (D + t(D)) / 2
-  # D = fixit(D, epsilon = eig_tol_D)
+  D = fixit(D, epsilon = eig_tol_D)
   max_D = max(abs(diag(D)))
   # D = D / max_D
-  # diag(D) = diag(D) + max_D * epsilon_D
-  diag(D) = diag(D) + epsilon_D
+  diag(D) = diag(D) + max_D * epsilon_D
+  # diag(D) = diag(D) + epsilon_D
   #################################### for test #######################################
   # alpha_mat = matrix(rnorm(n_l * n_class), n_l, n_class)
   # temp_vec = 0
