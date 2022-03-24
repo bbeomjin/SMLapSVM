af5a788ef1f351abff13afd48d9118d1697c1a48
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2022-03-24 13:00:07 +0900

    Update subfuncs.R

--- R/subfuncs.R
+++ R/subfuncs.R
@@ -476,8 +476,8 @@ fixit = function(A, epsilon = .Machine$double.eps) {
   delta = max(abs(v)) * epsilon
   # delta = epsilon
   # delta = 2 * d[1] * max(abs(v)) * epsilon
-  # tau = max(0, delta - v)
-  tau = pmax(0, delta - v)
+  tau = max(0, delta - v)
+  # tau = pmax(0, delta - v)
   A = eig$vectors %*% diag(v, d[1]) %*% t(eig$vectors) + diag(tau, d[1])
   # A = eig$vectors %*% ((v + tau) * t(eig$vectors))
   return(A)
