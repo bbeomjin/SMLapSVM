9427317ca0d8902b3e8599c2347aa49786ad2f97
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2022-03-28 15:11:06 +0900

    Update rmlapsvm.R

--- R/rmlapsvm.R
+++ R/rmlapsvm.R
@@ -30,6 +30,7 @@ rmlapsvm_compact = function(K, L, y, gamma = 0.5, lambda, lambda_I, epsilon = 1e
   # K = fixit(K, eig_tol_D)
   # inv_LK = inverse(diag(n_l * lambda, n) + n_l * lambda_I / n^2 * (L %*% K), epsilon = inv_tol)
   LK = diag(n_l * lambda, n) + n_l * lambda_I / n^2 * (L %*% K)
+  LK = fixit(LK, epsilon = eig_tol_D)
   # max_LK = max(abs(LK))
   # inv_LK = solve(LK + diag(max_LK * epsilon_I, n), t(J))
 
@@ -60,7 +61,7 @@ rmlapsvm_compact = function(K, L, y, gamma = 0.5, lambda, lambda_I, epsilon = 1e
     D = D + t(Hmatj[[j]]) %*% Q %*% Hmatj[[j]]
     Amat[j, ] = rep(1, n_l) %*% Hmatj[[j]]
   }
-  # D = fixit(D, epsilon = eig_tol_D)
+  D = fixit(D, epsilon = eig_tol_D)
   max_D = max(abs(D))
   # D = D / max_D
   diag(D) = diag(D) + max_D * epsilon_D
